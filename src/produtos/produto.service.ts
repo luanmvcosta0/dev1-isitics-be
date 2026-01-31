9c0b91da48e542e240cc3cd3d57543c8653b948c
luanmvcosta0 <luanmvcosta@gmail.com>
2026-01-31 19:07:09 -0300

    feat: adicionando metodo findOnde no service

--- src/produtos/produto.service.ts
+++ src/produtos/produto.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Get, Injectable } from "@nestjs/common";
 import { Repository } from "typeorm";
 import { Produto } from "./entites/produto.entity";
 import { InjectRepository } from "@nestjs/typeorm";
@@ -18,6 +18,14 @@ export class ProdutoService {
 
   async findAll(): Promise<Produto[]> {
     return this.produtoRepository.find()
+  }
+
+  async findOne(id: number): Promise<Produto> {
+    const produto = await this.produtoRepository.findOne({
+      where: {id_produto: id},
+    })
+
+    return produto;
   }
 
 }
