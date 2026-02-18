304010dd8784742a1172fc3f522e24cc01e2ae13
luanmvcosta0 <luanmvcosta@gmail.com>
2026-02-18 11:55:27 -0300

    feat: habilitar autoLoadEntities e sincronização na configuração do banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,6 +16,8 @@ import { ConfigModule } from '@nestjs/config';
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
