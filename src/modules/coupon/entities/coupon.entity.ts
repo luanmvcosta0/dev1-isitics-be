10290887e8095d6df30b8ed51c4d3a9f4e791c79
luanmvcosta0 <luanmvcosta@gmail.com>
2026-03-15 10:02:29 -0300

    fix: corrigindo validação regex

--- src/modules/coupon/entities/coupon.entity.ts
+++ src/modules/coupon/entities/coupon.entity.ts
@@ -1,11 +1,12 @@
-import { Length } from 'class-validator';
+import { Length, Matches } from 'class-validator';
 import { Column } from 'typeorm';
 
 export class CouponEntity {
   @Column({ name: 'code', length: 20, nullable: false, unique: true })
   @Length(4, 20, {
-    message: 'O campo code deve ter no mínimo 4 caracteres e máximo 20',
+    message: 'O código deve ter entre 4 e 20 caracteres',
   })
+  @Matches(/^[a-zA-Z0-9]+$/)
   code: string;
 
   type: string;
