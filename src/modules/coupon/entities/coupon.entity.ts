988744c0778f7fe0407411c7c60cea953495c047
luanmvcosta0 <luanmvcosta@gmail.com>
2026-03-15 09:39:18 -0300

    feat: adicionando validação de caracteres minimos e maximos

--- src/modules/coupon/entities/coupon.entity.ts
+++ src/modules/coupon/entities/coupon.entity.ts
@@ -1,7 +1,11 @@
+import { Length } from 'class-validator';
 import { Column } from 'typeorm';
 
 export class CouponEntity {
   @Column({ name: 'code', nullable: false, unique: true })
+  @Length(4, 20, {
+    message: 'O campo code deve ter no mínimo 4 caracteres e máximo 20',
+  })
   code: string;
   type: string;
   value: number;
