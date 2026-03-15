9122f5aa2e4d4c9ee0e9e51e7e79b46a239156f7
luanmvcosta0 <luanmvcosta@gmail.com>
2026-03-15 10:35:31 -0300

    feat: criando dto e deixando validações para o dto

--- src/modules/coupon/entities/coupon.entity.ts
+++ src/modules/coupon/entities/coupon.entity.ts
@@ -1,14 +1,7 @@
-import { Length, Matches } from 'class-validator';
 import { Column } from 'typeorm';
 
 export class CouponEntity {
   @Column({ name: 'code', length: 20, nullable: false, unique: true })
-  @Length(4, 20, {
-    message: 'O código deve ter entre 4 e 20 caracteres',
-  })
-  @Matches(/^[a-zA-Z0-9]+$/)
-  code: string;
-
   type: string;
   value: number;
   oneShot: boolean;
