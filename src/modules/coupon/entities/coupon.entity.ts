bdb2df37d51bcdded1ea2aa492a7ded223160287
luanmvcosta0 <luanmvcosta@gmail.com>
2026-03-15 09:44:10 -0300

    feat: adicionando validação lenth na entidade para ORM

--- src/modules/coupon/entities/coupon.entity.ts
+++ src/modules/coupon/entities/coupon.entity.ts
@@ -2,11 +2,12 @@ import { Length } from 'class-validator';
 import { Column } from 'typeorm';
 
 export class CouponEntity {
-  @Column({ name: 'code', nullable: false, unique: true })
+  @Column({ name: 'code', length: 20, nullable: false, unique: true })
   @Length(4, 20, {
     message: 'O campo code deve ter no mínimo 4 caracteres e máximo 20',
   })
   code: string;
+
   type: string;
   value: number;
   oneShot: boolean;
