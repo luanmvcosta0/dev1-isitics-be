536a747306e16bb95ae95f947fae65fea91e33d4
luanmvcosta0 <luanmvcosta@gmail.com>
2026-03-17 19:36:40 -0300

    fix: corrigindo campo para obrigatorio

--- src/modules/coupon/entities/coupon.entity.ts
+++ src/modules/coupon/entities/coupon.entity.ts
@@ -4,8 +4,9 @@ export class CouponEntity {
   @Column({ name: 'code', length: 20, nullable: false, unique: true })
   code: string;
 
-  @Column({ name: 'type', unique: true })
+  @Column({ name: 'type', nullable: false })
   type: string;
+
   value: number;
   oneShot: boolean;
   valid_from: Date;
