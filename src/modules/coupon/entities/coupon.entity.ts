d213e3b63469dd2670a0e56c2fef9749304f98b5
luanmvcosta0 <luanmvcosta@gmail.com>
2026-03-17 19:24:07 -0300

    fix: corrigindo coluna code

--- src/modules/coupon/entities/coupon.entity.ts
+++ src/modules/coupon/entities/coupon.entity.ts
@@ -2,6 +2,8 @@ import { Column } from 'typeorm';
 
 export class CouponEntity {
   @Column({ name: 'code', length: 20, nullable: false, unique: true })
+  code: string;
+
   type: string;
   value: number;
   oneShot: boolean;
