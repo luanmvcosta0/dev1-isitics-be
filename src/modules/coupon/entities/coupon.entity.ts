da0635c3af12daf9bd8e4496811da38410f91898
luanmvcosta0 <luanmvcosta@gmail.com>
2026-03-17 19:30:22 -0300

    feat: adicionando coluna type à entidade Coupon

--- src/modules/coupon/entities/coupon.entity.ts
+++ src/modules/coupon/entities/coupon.entity.ts
@@ -4,6 +4,7 @@ export class CouponEntity {
   @Column({ name: 'code', length: 20, nullable: false, unique: true })
   code: string;
 
+  @Column({ name: 'type', unique: true })
   type: string;
   value: number;
   oneShot: boolean;
